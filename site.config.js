72943dba88916529d4f8055c53bb1e4548edb4a6
heeju <heeju1223@khu.ac.kr>
2026-05-08 09:12:19 +0900

    환경변수 설정

--- site.config.js
+++ site.config.js
@@ -1,75 +1,75 @@
 const CONFIG = {
-  // profile setting (required)
-  profile: {
-    name: "heeju mo",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "developer",
-    bio: "Hello!!!",
-    email: "moheeju1223@gmail.com",
-    linkedin: "",
-    github: "heejudy",
-    instagram: "",
-  },
-  projects: [
-    {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
-    },
-  ],
-  // blog setting (required)
-  blog: {
-    title: "heeju-log",
-    description: "welcome to morethan-log!",
-    scheme: "dark", // 'light' | 'dark' | 'system'
-  },
+  // profile setting (required)
+  profile: {
+    name: "heeju mo",
+    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "developer",
+    bio: "Hello!!!",
+    email: "moheeju1223@gmail.com",
+    linkedin: "",
+    github: "heejudy",
+    instagram: "",
+  },
+  projects: [
+    {
+      name: `morethan-log`,
+      href: "https://github.com/morethanmin/morethan-log",
+    },
+  ],
+  // blog setting (required)
+  blog: {
+    title: "heeju-log",
+    description: "welcome to morethan-log!",
+    scheme: "dark", // 'light' | 'dark' | 'system'
+  },
 
-  // CONFIG configration (required)
-  link: "https://morethan-32qnxron4-heejudys-projects.vercel.app/",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
-  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+  // CONFIG configuration (required)
+  link: "https://morethan-32qnxron4-heejudys-projects.vercel.app",
+  since: 2022, // If leave this empty, current year will be used.
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
-  // notion configuration (required)
-  notionConfig: {
-    pageId: "8e89b942-cfa4-8300-bd02-87841c825fb1"
-  },
+  // notion configuration (required)
+  notionConfig: {
+    pageId: process.env.NOTION_PAGE_ID,
+  },
 
-  // plugin configuration (optional)
-  googleAnalytics: {
-    enable: false,
-    config: {
-      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
-    },
-  },
-  googleSearchConsole: {
-    enable: false,
-    config: {
-      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
-    },
-  },
-  naverSearchAdvisor: {
-    enable: false,
-    config: {
-      siteVerification: process.env.NEXT_PUBLIC_NAVER_SITE_VERIFICATION || "",
-    },
-  },
-  utterances: {
-    enable: false,
-    config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
-      "issue-term": "og:title",
-      label: "💬 Utterances",
-    },
-  },
-  cusdis: {
-    enable: false,
-    config: {
-      host: "https://cusdis.com",
-      appid: "", // Embed Code -> data-app-id value
-    },
-  },
-  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
-  revalidateTime: 21600 * 7, // revalidate time for [slug], index
+  // plugin configuration (optional)
+  googleAnalytics: {
+    enable: false,
+    config: {
+      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
+    },
+  },
+  googleSearchConsole: {
+    enable: false,
+    config: {
+      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
+    },
+  },
+  naverSearchAdvisor: {
+    enable: false,
+    config: {
+      siteVerification: process.env.NEXT_PUBLIC_NAVER_SITE_VERIFICATION || "",
+    },
+  },
+  utterances: {
+    enable: false,
+    config: {
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      "issue-term": "og:title",
+      label: "💬 Utterances",
+    },
+  },
+  cusdis: {
+    enable: false,
+    config: {
+      host: "https://cusdis.com",
+      appid: "", // Embed Code -> data-app-id value
+    },
+  },
+  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+  revalidateTime: 21600 * 7, // revalidate time for [slug], index
 }
 
 module.exports = { CONFIG }
