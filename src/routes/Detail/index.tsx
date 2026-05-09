7a2fecfa98946476e1e2957fbdc4747009d404ef
heeju <heeju1223@khu.ac.kr>
2026-05-09 15:51:35 +0900

    mermaid 완벽 제거

--- src/routes/Detail/index.tsx
+++ src/routes/Detail/index.tsx
@@ -1,4 +1,3 @@
-import useMermaidEffect from "./hooks/useMermaidEffect"
 import PostDetail from "./PostDetail"
 import PageDetail from "./PageDetail"
 import styled from "@emotion/styled"
@@ -9,7 +8,6 @@ type Props = {}
 
 const Detail: React.FC<Props> = () => {
   const data = usePostQuery()
-  useMermaidEffect()
 
   if (!data) return null
   const type = getFirstPropertyValue(data.type)
@@ -31,8 +29,7 @@ const StyledWrapper = styled.div`
     padding: 40px 0;
   }
   /** Reference: https://github.com/chriskempson/tomorrow-theme **/
-  code[class*="language-mermaid"],
-  pre[class*="language-mermaid"] {
+  {
     background-color: ${({ theme }) => theme.colors.gray5};
   }
 `
