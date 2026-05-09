115837672d680f6e7c2e7b25ca293566ebe5462f
heeju <heeju1223@khu.ac.kr>
2026-05-09 16:27:16 +0900

    에러 수정

--- src/routes/Feed/ServiceCard.tsx
+++ src/routes/Feed/ServiceCard.tsx
@@ -8,22 +8,23 @@ const ServiceCard: React.FC = () => {
   if (!CONFIG.projects) return null
   return (
     <>
-      <StyledTitle>
-        <Emoji>🌟</Emoji> Service
-      </StyledTitle>
-      <StyledWrapper>
-        {CONFIG.projects.map((project, idx) => (
-          <a
-            key={idx}
-            href={`${project.href}`}
-            rel="noreferrer"
-            target="_blank"
-          >
-            <AiFillCodeSandboxCircle className="icon" />
-            <div className="name">{project.name}</div>
-          </a>
-        ))}
-      </StyledWrapper>
+      <div className="p-1 mb-3 dark:text-white">🌟 Service</div>
+      <ul className="flex flex-col p-1 bg-white rounded-2xl mb-9 dark:bg-zinc-700">
+        {CONFIG.projects.map((project, index) => {
+          return (
+            <a
+              key={project.name}
+              href={`${project.href}`}
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center gap-3 p-3 text-gray-500 cursor-pointer hover:underline hover:underline-offset-2 rounded-2xl dark:text-white hover:text-black dark:hover:text-white"
+            >
+              <AiOutlineLink className="text-2xl" />
+              <div className="text-sm">{project.name}</div>
+            </a>
+          )
+        })}
+      </ul>
     </>
   )
 }
